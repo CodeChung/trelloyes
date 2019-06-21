a6a8d71db0bd9d6a8323b964911cc4413fd91d6c
Harry Chung <hc9825@gmail.com>
2019-06-21 16:08:16 -0700

    added tests

--- src/App.js
+++ src/App.js
@@ -3,20 +3,27 @@ import STORE from './store';
 import List from './List';
 import './App.css';
 
-function App() {
-  const list = STORE.lists.map((list, index) => {
-    return <List header={list.header} cards={list.cardIds} key={index}/>
-  })
-  return (
-    <div className="App">
-      <header className='App-header'>
-        <h1>Trelloyes!</h1>
-      </header>
-      <main className='App-list'>
-        {list}
-      </main>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    store: STORE,
+  }
+  render() {
+    const list = STORE.lists.map((list, index) => {
+      return <List header={list.header} cards={list.cardIds} key={index}/>
+    })
+    return (
+      <div className="App">
+        <header className='App-header'>
+          <h1>Trelloyes!</h1>
+        </header>
+        <main className='App-list'>
+          {list}
+        </main>
+      </div>
+    )
+  }
+    
+
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Card from './Card';
+import List from './List'
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+describe('Card component', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Card />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  })
+})
+
+describe('List component', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<List header=' ' cards={[]}/>, div);
+    ReactDOM.unmountComponentAtNode(div);
+  })
+})
